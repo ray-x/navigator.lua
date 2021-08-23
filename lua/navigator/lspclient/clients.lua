78e574cbcd6ff58abada6b033a0e3152e4d6de04
ray-x <rayx.cn@gmail.com>
2021-08-23 12:56:04 +1000

    codelens setup for gopls

--- lua/navigator/lspclient/clients.lua
+++ lua/navigator/lspclient/clients.lua
@@ -103,7 +103,9 @@ local setups = {
         analyses = {unusedparams = true, unreachable = false},
         codelenses = {
           generate = true, -- show the `go generate` lens.
-          gc_details = true --  // Show a code lens toggling the display of gc's choices.
+          gc_details = true, --  // Show a code lens toggling the display of gc's choices.
+          test = true,
+          tidy = true
         },
         usePlaceholders = true,
         completeUnimported = true,
