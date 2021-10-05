a952d694e1304a41b6b0f905dfaabbd7247bdb99
ray-x <rayx.cn@gmail.com>
2021-10-05 17:53:53 +1100

    lazyload for lua-dev

--- lua/navigator/lazyloader.lua
+++ lua/navigator/lazyloader.lua
@@ -1,35 +1,35 @@
 return {
   init = function()
-local loader = nil
-local packer_plugins = packer_plugins or nil -- suppress warnings
+    local loader = nil
+    local packer_plugins = packer_plugins or nil -- suppress warnings
     local log = require'navigator.util'.log
--- packer only
-if packer_plugins ~= nil then -- packer install
-  local lazy_plugins = {
-    ["nvim-lspconfig"] = "neovim/nvim-lspconfig",
-    ["guihua.lua"] = "ray-x/guihua.lua"
-  }
+    -- packer only
+    if packer_plugins ~= nil then -- packer install
+      local lazy_plugins = {
+        ["nvim-lspconfig"] = "neovim/nvim-lspconfig",
+        ["guihua.lua"] = "ray-x/guihua.lua"
+      }
 
-  if _NgConfigValues.lspinstall == true then
-    lazy_plugins["nvim-lspinstall"] = "kabouzeid/nvim-lspinstall"
-  end
+      if _NgConfigValues.lspinstall == true then
+        lazy_plugins["nvim-lspinstall"] = "kabouzeid/nvim-lspinstall"
+      end
 
-  -- packer installed
-  loader = require"packer".loader
-  for plugin, url in pairs(lazy_plugins) do
-    if not packer_plugins[url] or not packer_plugins[url].loaded then
+      -- packer installed
+      loader = require"packer".loader
+      for plugin, url in pairs(lazy_plugins) do
+        if not packer_plugins[url] or not packer_plugins[url].loaded then
           -- log("loading ", plugin)
-      loader(plugin)
-    end
-  end
+          loader(plugin)
+        end
+      end
 
     end
 
-  if _NgConfigValues.lspinstall == true then
-    local has_lspinst, lspinst = pcall(require, "lspinstall")
+    if _NgConfigValues.lspinstall == true then
+      local has_lspinst, lspinst = pcall(require, "lspinstall")
       log('lspinstall', has_lspinst)
-    if has_lspinst then
-      lspinst.setup()
+      if has_lspinst then
+        lspinst.setup()
         local configs = require "lspconfig/configs"
         local servers = require'lspinstall'.installed_servers()
         for _, server in pairs(servers) do
@@ -39,9 +39,9 @@ if packer_plugins ~= nil then -- packer install
             lsp_inst_cfg = lsp_inst_cfg.document_config.default_config
             lsp_inst_cfg = vim.tbl_deep_extend('keep', lsp_inst_cfg, cfg)
             require'lspconfig'[server].setup(lsp_inst_cfg)
-    end
-  end
-end
+          end
+        end
+      end
     end
   end,
   load = function(plugin_name, path)
