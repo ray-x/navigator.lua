1b7fb9845b2b01f1ca2fd76d706430873d7257d7
ray-x <rayx.cn@gmail.com>
2022-06-27 20:00:51 +1000

    show side panel

--- lua/navigator/hierarchy.lua
+++ lua/navigator/hierarchy.lua
@@ -8,13 +8,18 @@ local lsphelper = require('navigator.lspwrapper')
 local path_sep = require('navigator.util').path_sep()
 local path_cur = require('navigator.util').path_cur()
 local cwd = vim.loop.cwd()
+local in_method = 'callHierarchy/incomingCalls'
+local out_method = 'callHierarchy/outgoingCalls'
+
+local lsp_method = { to = out_method, from = in_method }
+local panel_method = { to = out_method, from = in_method }
+
 local M = {}
 local outgoing_calls_handler
 local incoming_calls_handler
 local hierarchy_handler
 
-local outgoing_calls_panel_creator
-local incoming_calls_panel_creator
+local call_hierarchy
 
 local function pick_call_hierarchy_item(call_hierarchy_items)
   if not call_hierarchy_items then
@@ -36,6 +41,7 @@ local function pick_call_hierarchy_item(call_hierarchy_items)
 end
 
 local function call_hierarchy_result_procesor(direction, err, result, ctx, config)
+  math.randomseed(os.clock() * 100000000000)
   log(direction, err, result, ctx, config)
   trace(result)
   if not result then
@@ -44,8 +50,8 @@ local function call_hierarchy_result_procesor(direction, err, result, ctx, confi
   end
   -- trace('call_hierarchy', result)
 
-  local bufnr = vim.api.nvim_get_current_buf()
-  assert(next(vim.lsp.buf_get_clients(bufnr)), 'Must have a client running to use lsp_tags')
+  local bufnr = ctx.bufnr or vim.api.nvim_get_current_buf()
+  assert(next(vim.lsp.buf_get_clients(bufnr)), 'Must have a client running to use call hierarchy')
   if err ~= nil then
     log('dir', direction, 'result', result, 'err', err, ctx)
     vim.notify('ERROR: ' .. err, vim.lsp.log_levels.WARN)
@@ -70,40 +76,16 @@ local function call_hierarchy_result_procesor(direction, err, result, ctx, confi
     disp_item = vim.tbl_deep_extend('force', disp_item, {
       filename = filename,
       display_filename = display_filename,
-      indent_level = ctx.depth,
+      indent_level = ctx.depth or 1,
+      method = lsp_method[direction],
       node_text = call_hierarchy_item.name,
       type = kind,
+      id = math.random(1, 100000),
       text = kind .. call_hierarchy_item.name .. ' ﰲ ' .. call_hierarchy_item.detail,
       lnum = call_hierarchy_item.selectionRange.start.line + 1,
       col = call_hierarchy_item.selectionRange.start.character,
     })
     table.insert(items, disp_item)
-    -- if ctx.depth or 0 > 0 then
-    --   local params = {
-    --     position = {
-    --       character = disp_item.selectionRange.start.character,
-    --       line = disp_item.selectionRange.start.line,
-    --     },
-    --     textDocument = {
-    --       uri = disp_item.uri,
-    --     },
-    --   }
-    --   local api = 'callHierarchy/outgoingCalls'
-    --   local handler = outgoing_calls_handler
-    --   if direction == 'incoming' then
-    --     api = 'callHierarchy/incomingCalls'
-    --     handler = incoming_calls_handler
-    --   end
-    --   lsphelper.call_sync(
-    --     api,
-    --     params,
-    --     ctx,
-    --     vim.lsp.with(
-    --       partial(handler, 0),
-    --       { depth = ctx.depth - 1, direction = 'to', items = ctx.items, no_show = true }
-    --     )
-    --   )
-    -- end
   end
   log(items)
   return items
@@ -125,32 +107,13 @@ hierarchy_handler = function(dir, handler, show, api, err, result, ctx, cfg)
   if ctx.no_show then
     return results
   end
+  -- local panel = args.panel
+  -- local items = args.items
+  -- local parent_node = args.node
+  -- local section_id = args.section_id or 1
   local show_args = { items = results, ft = ft, api = api, bufnr = bufnr }
-  local win = show({ items = results, ft = ft, api = api, bufnr = bufnr })
+  local win = show(show_args)
   return results, win
-end
-
-function M.get_children(params, direction, callback)
-  vim.lsp.buf_request(nil, 'textDocument/prepareCallHierarchy', params, function(err, result)
-    if err then
-      vim.notify('Prepare error' .. tostring(err), vim.log.levels.ERROR, {
-        title = 'Hierarchy prepare',
-      })
-      return
-    end
-
-    local call_hierarchy_item = pick_call_hierarchy_item(result)
-
-    local method = 'callHierarchy/incomingCalls'
-    local title = 'LSP Incoming Calls'
-
-    if direction == 'outgoing' then
-      method = 'callHierarchy/outgoingCalls'
-      title = 'LSP Outgoing Calls'
-    end
-
-    M.call_hierarchy({}, method, title, call_hierarchy_item, callback)
-  end)
 end
 
 local make_params = function(uri, pos)
@@ -160,39 +123,6 @@ local make_params = function(uri, pos)
     },
     position = pos,
   }
-end
-function M.expand(node)
-  local line = vim.api.nvim_exec("echo line('.')", true)
-
-  local params = make_params(node.uri, {
-    line = node.range.start.line,
-    character = node.range.start.character,
-  })
-
-  H.get_children(params, t.direction, function(result)
-    node.status = 'open'
-    if result ~= nil and #result > 0 then -- no incoming
-      for _, item in ipairs(result) do
-        local child
-        if t.direction == 'outgoing' then
-          child = t.create_node(item.to.name, item.to.kind, item.to.uri, item.to.detail, item.to.range, item.fromRanges)
-        else
-          child = t.create_node(
-            item.from.name,
-            item.from.kind,
-            item.from.uri,
-            item.from.detail,
-            item.from.range,
-            item.fromRanges
-          )
-        end
-
-        table.insert(node.children, child)
-      end
-    end
-    w.create_window()
-    vim.cmd('execute  "normal! ' .. line .. 'G"')
-  end)
 end
 
 local function display_panel(args)
@@ -209,16 +139,48 @@ local function display_panel(args)
     render = function(bufnr)
       return items
     end,
-    fold = function(node)
-      if node.expanded or node.expandable then
-        return vim.cmd('normal! za')
-      end
+    fold = function(panel, node)
+      -- if node.expanded or node.expandable then
+      --   return vim.cmd('normal! za')
+      -- end
       -- new node
-      M.expand()
+      log('fold')
+      expand(panel, node)
       return node
     end,
   })
   p:open(true)
+end
+
+local function expand_item(args)
+  -- args = {items=results, ft=ft, api=api}
+  print('dispaly panel')
+  log(args)
+  local panel = args.panel
+  local items = args.items
+  local parent_node = args.node
+  local section_id = args.section_id or 1
+
+  local sect
+  for _, s in pairs(panel.sections) do
+    if s.id == section_id then
+      sect = s
+      break
+    end
+  end
+  if not sect then
+    -- no setion
+    sect = panel.sections[1]
+  end
+  local nodes = sect.nodes
+  for i, node in pairs(nodes) do
+    if node.id == parent_node.id then
+      table.insert(nodes, i, args.items)
+      break
+    end
+  end
+  -- render the panel again
+  panel:redraw(false)
 end
 
 incoming_calls_handler = util.partial4(
@@ -239,17 +201,47 @@ local incoming_calls_panel = util.partial4(
 )
 local outgoing_calls_panel = util.partial4(hierarchy_handler, 'to', call_hierarchy_handler_to, display_panel, ' ')
 
-local function request(method, params, handler)
-  return vim.lsp.buf_request(0, method, params, handler)
+local incoming_calls_expand = util.partial4(hierarchy_handler, 'from', call_hierarchy_handler_from, expand_item, ' ')
+local outgoing_calls_expand = util.partial4(hierarchy_handler, 'to', call_hierarchy_handler_to, expand_item, ' ')
+
+function expand(panel, node)
+  log(panel)
+  log(node)
+  local params = make_params(node.uri, {
+    line = node.range.start.line,
+    character = node.range.start.character,
+  })
+  local handler = incoming_calls_expand
+  if node.api == out_method then
+    handler = outgoing_calls_expand
+  end
+
+  local bufnr = vim.uri_to_bufnr(node.uri)
+  call_hierarchy(node.method, {
+    params = params,
+    panel = panel,
+    indent_level = node.indent_level + 1,
+    handler = handler,
+    bufnr = bufnr,
+  })
 end
 
+local request = vim.lsp.buf_request
+
 -- call_hierarchy with floating window
-local function call_hierarchy(method, opts)
-  local params = vim.lsp.util.make_position_params()
+call_hierarchy = function(method, opts)
+  trace(method, opts)
   opts = opts or {}
+  local params = opts.params or vim.lsp.util.make_position_params()
   local handler = opts.handler -- we can pass in customer handler
-  log(opts)
+  local bufnr = opts.bufnr
+  -- handler = function(err, result, ctx, cfg)
+  --   ctx.opts = opts
+  --   return handler(err, result, ctx, cfg)
+  -- end
+  -- log(opts, params)
   request(
+    bufnr,
     'textDocument/prepareCallHierarchy',
     params,
     vim.lsp.with(function(err, result, ctx)
@@ -258,22 +250,22 @@ local function call_hierarchy(method, opts)
         return
       end
       local call_hierarchy_item = pick_call_hierarchy_item(result)
-      log('result', result, 'items', call_hierarchy_item)
       local client = vim.lsp.get_client_by_id(ctx.client_id)
       if client then
-        client.request(method, { item = call_hierarchy_item, args = { dir = 'in' } }, handler, ctx.bufnr)
+        trace('result', result, 'items', call_hierarchy_item, method, ctx, client.name)
+        client.request(method, {
+          item = call_hierarchy_item,
+          args = {
+            method = method,
+            panel = opts.panel,
+          },
+        }, handler, ctx.bufnr)
       else
-        vim.notify(
-          string.format('Client with id=%d disappeared during call hierarchy request', ctx.client_id),
-          vim.log.levels.WARN
-        )
+        vim.notify(string.format('Client with id=%d stopped', ctx.client_id), vim.log.levels.WARN)
       end
     end, { direction = method, depth = opts.depth })
   )
 end
-
-local in_method = 'callHierarchy/incomingCalls'
-local out_method = 'callHierarchy/outgoingCalls'
 
 function M.incoming_calls(opts)
   call_hierarchy(in_method, opts)
