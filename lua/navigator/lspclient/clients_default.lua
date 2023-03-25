ab42ffc5c68d3914e72c2e53d81d5909ccb7f868
ray-x <rayx.cn@gmail.com>
2023-03-26 02:45:00 +1100

    issue #266 Error when opening Go files

--- lua/navigator/lspclient/clients_default.lua
+++ lua/navigator/lspclient/clients_default.lua
@@ -92,11 +92,16 @@ M.defaults = function()
           and not client.server_capabilities.semanticTokensProvider
         then
           local semantic = client.config.capabilities.textDocument.semanticTokens
-          client.server_capabilities.semanticTokensProvider = {
-            full = true,
-            legend = { tokenModifiers = semantic.tokenModifiers, tokenTypes = semantic.tokenTypes },
-            range = true,
-          }
+          if semantic then
+            client.server_capabilities.semanticTokensProvider = {
+              full = true,
+              legend = {
+                tokenModifiers = semantic.tokenModifiers,
+                tokenTypes = semantic.tokenTypes,
+              },
+              range = true,
+            }
+          end
         end
       end,
       root_dir = function(fname)
